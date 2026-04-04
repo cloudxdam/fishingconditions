12822f14e9d33404861e1227c8234ea039371d98
cloudxdam <cloudxdam@gmail.com>
2026-04-04 23:31:23 +0100

    Initial commit

---
+++ app/src/main/java/com/pachedev/fishingconditions/MainActivity.java
@@ -0,0 +1,15 @@
+package com.pachedev.fishingconditions;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+    }
+}
